8f342c704a99ab12168d2d228282355b03a1e62c
vivifds <vivianefds@gmail.com>
2021-11-11 18:47:17 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    // Integer, strings, character (literals)
+    val numero: Int = 2021
+    val texto: String = "Olá, Deevas!"
+    val caractere: Char = 'B'
+    val espaco: Char = ' '
+    var boolean = true
+
+     boolean = false
+
+    println("numero; $numero")
+    println("texto; $texto")
+    println("caractere; $caractere")
+    println("espaco; $espaco")
+    println("boolena; $boolean")
+
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
